e1132a9d6181121c331c7a31bbc5269ee0a36543
Chris Redpath <chris.redpath@arm.com>
2017-03-27 18:20:20 +0100

    sched/tune: don't use schedtune before it is ready
    When EAS is enabled during boot, we have to be careful not to use
    schedtune from fair.c before it is ready or it will warn us and we'll
    get a traceback in the console.
    
    Change-Id: I1a5cf29b18af626545c636c51219f9ed497c19fa
    Signed-off-by: Chris Redpath <chris.redpath@arm.com>
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>
    (cherry picked from commit 7a97582a04c3d2ddbb83eaad3fcb39bbf20f2cb2)
    (cherry picked from commit 344eeddd9fcb2e59c02974d4e8d22f761104f3dd)

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5384,6 +5384,7 @@ static inline int
 normalize_energy(int energy_diff)
 {
 	u32 normalized_nrg;
+
 	/* during early setup, we don't know the extents */
 	if (unlikely(!schedtune_initialized))
 		return energy_diff < 0 ? -1 : 1 ;
